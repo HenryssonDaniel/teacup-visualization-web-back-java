5a0b37f7a079056e272025a5b6fe7b2473500943
Daniel Henrysson <henrysson.daniel@gmail.com>
2019-08-14 14:15:04 -0500

    Add recover tests
    Added tests for recover.

--- src/test/java/io/github/henryssondaniel/teacup/visualization/back/v1/_0/AccountTest.java
+++ src/test/java/io/github/henryssondaniel/teacup/visualization/back/v1/_0/AccountTest.java
@@ -7,6 +7,8 @@ import static javax.ws.rs.core.Response.Status.OK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.startsWith;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -14,6 +16,7 @@ import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
+import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
@@ -24,6 +27,9 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Transport;
 import javax.servlet.http.HttpSession;
 import javax.ws.rs.core.Response;
 import org.json.JSONObject;
@@ -41,12 +47,18 @@ class AccountTest {
   private static final String JSON_ID = "\"id\": \"123\"";
   private static final String JSON_LAST_NAME = "\"lastName\": \"lastName\"";
   private static final String LAST_NAME = "lastName";
+  private static final String RECOVER = "Recover";
+  private static final String RECOVER_CODE = "The recover code: ";
   private static final String SECRET = "password";
+  private static final String SMTP_HOST = "smtp.host";
+  private static final String SMTP_PORT = "smtp.port";
   private static final String TOKEN = "token";
   private static final String VISUALIZATION = "service.visualization";
 
+  private final Algorithm algorithm = mock(Algorithm.class);
   private final Claim claim = mock(Claim.class);
   private final DecodedJWT decodedJWT = mock(DecodedJWT.class);
+  private final EmailClient emailClient = mock(EmailClient.class);
   private final HttpClient httpClient = mock(HttpClient.class);
   private final HttpSession httpSession = mock(HttpSession.class);
   private final JSONObject jsonObject = mock(JSONObject.class);
@@ -70,6 +82,8 @@ class AccountTest {
     when(jsonObject.getString(TOKEN)).thenReturn(TOKEN);
     when(jwtVerifier.verify(TOKEN)).thenReturn(decodedJWT);
     when(properties.getProperty(VISUALIZATION)).thenReturn("http://localhost");
+    when(properties.getProperty(SMTP_HOST)).thenReturn("localhost");
+    when(properties.getProperty(SMTP_PORT)).thenReturn("8080");
   }
 
   @Test
@@ -403,6 +417,145 @@ class AccountTest {
 
     assertThat(new AccountImpl(null, httpClient, properties).logIn(EMAIL, httpSession, SECRET))
         .isEqualTo(NO_CONTENT.getStatusCode());
+
+    verify(httpClient).send(any(HttpRequest.class), eq(BodyHandlers.ofString()));
+    verifyNoMoreInteractions(httpClient);
+
+    verify(httpResponse).statusCode();
+    verifyNoMoreInteractions(httpResponse);
+
+    verifyZeroInteractions(httpSession);
+
+    verify(properties).getProperty(VISUALIZATION);
+    verifyNoMoreInteractions(properties);
+  }
+
+  @Test
+  void recover() throws IOException, InterruptedException, MessagingException {
+    assertThat(new AccountImpl(emailClient, httpClient, properties).recover(algorithm, EMAIL))
+        .isEqualTo(OK.getStatusCode());
+
+    verify(algorithm).getName();
+    verify(algorithm).getSigningKeyId();
+    verify(algorithm).sign(any(byte[].class), any(byte[].class));
+    verifyNoMoreInteractions(algorithm);
+
+    verify(emailClient)
+        .send(
+            startsWith(RECOVER_CODE),
+            any(Message.class),
+            eq(RECOVER),
+            eq(EMAIL),
+            any(Transport.class));
+    verifyNoMoreInteractions(emailClient);
+
+    verify(httpClient).send(any(HttpRequest.class), eq(BodyHandlers.ofString()));
+    verifyNoMoreInteractions(httpClient);
+
+    verify(httpResponse).statusCode();
+    verifyNoMoreInteractions(httpResponse);
+
+    verifyZeroInteractions(httpSession);
+
+    verify(properties).getProperty(VISUALIZATION);
+    verify(properties).getProperty(SMTP_HOST);
+    verify(properties).getProperty(SMTP_PORT);
+    verifyNoMoreInteractions(properties);
+  }
+
+  @Test
+  void recoverWhenEmailError() throws IOException, InterruptedException, MessagingException {
+    doThrow(new MessagingException("test"))
+        .when(emailClient)
+        .send(
+            startsWith(RECOVER_CODE),
+            any(Message.class),
+            eq(RECOVER),
+            eq(EMAIL),
+            any(Transport.class));
+
+    assertThat(new AccountImpl(emailClient, httpClient, properties).recover(algorithm, EMAIL))
+        .isEqualTo(OK.getStatusCode());
+
+    verify(algorithm).getName();
+    verify(algorithm).getSigningKeyId();
+    verify(algorithm).sign(any(byte[].class), any(byte[].class));
+    verifyNoMoreInteractions(algorithm);
+
+    verify(emailClient)
+        .send(
+            startsWith(RECOVER_CODE),
+            any(Message.class),
+            eq(RECOVER),
+            eq(EMAIL),
+            any(Transport.class));
+    verifyNoMoreInteractions(emailClient);
+
+    verify(httpClient).send(any(HttpRequest.class), eq(BodyHandlers.ofString()));
+    verifyNoMoreInteractions(httpClient);
+
+    verify(httpResponse).statusCode();
+    verifyNoMoreInteractions(httpResponse);
+
+    verifyZeroInteractions(httpSession);
+
+    verify(properties).getProperty(VISUALIZATION);
+    verify(properties).getProperty(SMTP_HOST);
+    verify(properties).getProperty(SMTP_PORT);
+    verifyNoMoreInteractions(properties);
+  }
+
+  @Test
+  void recoverWhenInterruptedException() throws IOException, InterruptedException {
+    when(httpClient.send(any(HttpRequest.class), eq(BodyHandlers.ofString())))
+        .thenThrow(new InterruptedException("test"));
+
+    assertThat(new AccountImpl(emailClient, httpClient, properties).recover(algorithm, EMAIL))
+        .isEqualTo(INTERNAL_SERVER_ERROR.getStatusCode());
+
+    verifyZeroInteractions(algorithm);
+    verifyZeroInteractions(emailClient);
+
+    verify(httpClient).send(any(HttpRequest.class), eq(BodyHandlers.ofString()));
+    verifyNoMoreInteractions(httpClient);
+
+    verifyZeroInteractions(httpResponse);
+    verifyZeroInteractions(httpSession);
+
+    verify(properties).getProperty(VISUALIZATION);
+    verifyNoMoreInteractions(properties);
+  }
+
+  @Test
+  void recoverWhenIoException() throws IOException, InterruptedException {
+    when(httpClient.send(any(HttpRequest.class), eq(BodyHandlers.ofString())))
+        .thenThrow(new IOException("test"));
+
+    assertThat(new AccountImpl(emailClient, httpClient, properties).recover(algorithm, EMAIL))
+        .isEqualTo(INTERNAL_SERVER_ERROR.getStatusCode());
+
+    verifyZeroInteractions(algorithm);
+    verifyZeroInteractions(emailClient);
+
+    verify(httpClient).send(any(HttpRequest.class), eq(BodyHandlers.ofString()));
+    verifyNoMoreInteractions(httpClient);
+
+    verifyZeroInteractions(httpResponse);
+    verifyZeroInteractions(httpSession);
+
+    verify(properties).getProperty(VISUALIZATION);
+    verifyNoMoreInteractions(properties);
+  }
+
+  @Test
+  void recoverWhenNotOk() throws IOException, InterruptedException {
+    when(httpResponse.statusCode()).thenReturn(NO_CONTENT.getStatusCode());
+
+    assertThat(new AccountImpl(emailClient, httpClient, properties).recover(algorithm, EMAIL))
+        .isEqualTo(NO_CONTENT.getStatusCode());
+
+    verifyZeroInteractions(algorithm);
+    verifyZeroInteractions(emailClient);
 
     verify(httpClient).send(any(HttpRequest.class), eq(BodyHandlers.ofString()));
     verifyNoMoreInteractions(httpClient);
